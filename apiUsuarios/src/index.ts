907b541486fa8a1d5d4499aa1de2bea30b2299a9
eusebio suarez <eusebiosuaresmartines@gmail.com>
2025-06-16 14:50:25 -0500

    implementacion de cors

--- apiUsuarios/src/index.ts
+++ apiUsuarios/src/index.ts
@@ -1,21 +1,25 @@
-import express from "express"
-import dotenv from "dotenv"
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors"; // <--- IMPORTAR CORS
 import registerRoutes from "./routes/registerRoutes";
-import loginRoutes from "./routes/loginRoutes"
-import logoutRoutes from "./routes/logoutRoutes"
-import cookieparser from "cookie-parser"
+import loginRoutes from "./routes/loginRoutes";
+import logoutRoutes from "./routes/logoutRoutes";
+import cookieparser from "cookie-parser";
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
-const PORT = process.env.PORT || 3000
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.use(express.json())
-app.use(cookieparser())
+// Habilita CORS para todos los orígenes
+app.use(cors()); // <--- PERMITE CUALQUIER ORIGEN
 
-app.use("/api/registrar", registerRoutes)
-app.use("/api/iniciarSesion", loginRoutes)
-app.use("/api/cerrarSesion", logoutRoutes)
+app.use(express.json());
+app.use(cookieparser());
+
+app.use("/api/registrar", registerRoutes);
+app.use("/api/iniciarSesion", loginRoutes);
+app.use("/api/cerrarSesion", logoutRoutes);
 
 app.listen(PORT, () => {
   console.log(`✅ Servidor corriendo en el puerto ${PORT}`);
