6eb70bd44de57f68be9768686b417e2a4487ffc6
eusebio suarez <eusebiosuaresmartines@gmail.com>
2025-06-20 22:29:24 -0500

    modificacion y fusion de apis

--- apiCarrito/src/types/usertypes.ts
+++
@@ -1,9 +0,0 @@
-import { RowDataPacket } from "mysql2/promise";
-
-//interfas de usuario
- export interface user extends RowDataPacket {
-  id: number;
-  nombre_usuario: string;
-  correo: string;
-  rol: string
-}
