f7157a3376b60b8a4423cd9a61da4c3fda810dba
emmanuel <emmanueltapasco07@gmail.com>
2025-06-27 08:49:11 -0500

    eliminacion de archivos

--- src/models/librosModels.ts
+++
@@ -1,6 +0,0 @@
-import conecion from './db'; // o '../db' si está en otro directorio
-
-export async function getLibros() {
-  const [rows] = await conecion.query('SELECT * FROM libros');
-  return rows;
-}
