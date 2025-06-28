622532cf744d18e29497ea80aed7119b1036f120
emmanuel <emmanueltapasco07@gmail.com>
2025-06-28 18:27:57 -0500

    cambios en la peticion de sql para actulizar libros 2

--- apisInesita/src/models/librosModels/updateLibrosModels.ts
+++ apisInesita/src/models/librosModels/updateLibrosModels.ts
@@ -10,7 +10,7 @@ export const validatLibro = async(nombre_libro:string):Promise<boolean>=>{
 }
 
 
-export const updateLibro = async(nombre_libro:string, precio:number, descripcion:string, stock:number, estado:number, foto:string):Promise<boolean>=>{
-    const [rows] = await coneccion.query<ResultSetHeader>("UPDATE libros_recetas SET nombre_libro = ?, precio = ?, descripcion = ?, stock = ?, estado = ?, foto = ? WHERE id_libro = ?", [nombre_libro, precio, descripcion, stock, estado, foto])
+export const updateLibro = async(id_libro: number, nombre_libro:string, precio:number, descripcion:string, stock:number, estado:number, foto:string):Promise<boolean>=>{
+    const [rows] = await coneccion.query<ResultSetHeader>("UPDATE libros_recetas SET nombre_libro = ?, precio = ?, descripcion = ?, stock = ?, estado = ?, foto = ? WHERE id_libro = ?", [nombre_libro, precio, descripcion, stock, estado, foto, id_libro])
     return rows.affectedRows > 0
 }
