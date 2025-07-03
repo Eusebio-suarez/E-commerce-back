347c6c53869232a5bb856552ce0129293ab6b097
eusebio suarez <eusebiosuaresmartines@gmail.com>
2025-07-03 07:45:36 -0500

    actualizar endpoint para traer rol del usuario

--- apisInesita/src/models/userModels/getUserModel.ts
+++ apisInesita/src/models/userModels/getUserModel.ts
@@ -1,7 +1,7 @@
 import coneccion from "../../config/db";
 
 export const getUsers = async ()=>{
-    const [rows] = await coneccion.query("select id_usuario, nombre_usuario,correo,fecha_creacion from  usuarios order by id_usuario desc;") 
+    const [rows] = await coneccion.query("select id_usuario, nombre_usuario, rol ,correo,fecha_creacion from  usuarios order by id_usuario desc;") 
 
     return rows
 }
